70de4e714f2f037fd2bfbc956900ab709277f901
Alexandro39 <144172367+Alexandro39@users.noreply.github.com>
2023-09-23 00:20:24 +0200

    lp

---
+++ NewHomeWork3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Введите целое число:");
+//int number = int.Parse(Console.ReadLine());
+//int res = number % 8;
+//Console.WriteLine(res);
+
+Console.WriteLine("Введите первое число;");
+int number1= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int number3 = int.Parse(Console.ReadLine());
+Console.WriteLine((number1 + number2 + number3));
+Console.WriteLine((number1 * number2 * number3));
